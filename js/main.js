7065830f87192f88a480d68dd3d0ac789d1d7744
cassiopeia <cassiopeia.neely@gmail.com>
2020-05-25 23:49:54 -0400

    button updates
    » Start button now changes the body background color and hides the start button itself, on click

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ introBox = document.getElementById('intro-box');
 storyOptions = document.getElementById('story-options');
 
 startButton.onclick = () => {
+    document.body.style.backgroundColor = '#0a151b';
     introBox.style.display = 'none';
     storyOptions.style.display = 'block';
+    startButton.style.display = 'none';
 };
