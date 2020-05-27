bb72b40864218071d2bb519fb93254d32e8836a3
cassiopeia <cassiopeia.neely@gmail.com>
2020-05-26 20:55:31 -0400

    hover or click?
    » Still deciding if the dropdowns should appear on click or hover

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 startButton = document.getElementById('start-button');
 introBox = document.getElementById('intro-box');
 storyOptions = document.getElementById('story-options');
+arrow = document.querySelector('.arrow');
+selectHolder = document.querySelector('.selection-placeholder');
+dropdownOptions = document.querySelector('.dropdown-options');
 
 startButton.onclick = () => {
     document.body.style.backgroundColor = '#0a151b';
@@ -8,3 +11,12 @@ startButton.onclick = () => {
     storyOptions.style.display = 'block';
     startButton.style.display = 'none';
 };
+
+// selectHolder.onclick = () => {
+//     arrow.style.transform = 'rotate(225deg)';
+//     arrow.style.margin = '0 0 0 10px';
+// };
+
+// selectHolder.onclick = () => {
+//     dropdownOptions.style.display = 'block';
+// };
