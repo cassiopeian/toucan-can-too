64aab460056bad76c4ccb0b97484560e776d3e2a
cassiopeia <cassiopeia.neely@gmail.com>
2020-06-03 14:11:28 -0400

    green circles
    » Selected letters are now indicated by green circles

--- js/search-script.js
+++ js/search-script.js
@@ -10,3 +10,35 @@ function assignLetter() {
     let randomize = Math.floor(Math.random() * letters.length);
     return letters[randomize];
 };
+
+
+// CHANGE COLOR WHEN CLICKED
+
+$('.letter').on('click', function() {
+    if ($(this).hasClass('selected-letter')) {
+        $(this).removeClass('selected-letter');
+    } else {
+        $(this).addClass('selected-letter');
+    }
+});
+
+$('.random-letter').on('click', function() {
+    if ($(this).hasClass('selected-letter')) {
+        $(this).removeClass('selected-letter');
+    } else {
+        $(this).addClass('selected-letter');
+    }
+});
+
+
+
+
+
+
+// let letter = document.getElementsByClassName('letter');
+
+// let oneOne = document.getElementById('1-1');
+// let oneTwo = document.getElementById('1-2');
+// let oneThree = document.getElementById('1-3');
+
+// let correctLetters = [oneOne, oneTwo, oneThree];
