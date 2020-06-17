d9be74f925b241983d567c7506eaaef7fe5bd438
cassiopeia <cassiopeia.neely@gmail.com>
2020-06-16 22:11:53 -0400

    hamburger menu functions
    » Sloth menu now opens and closes

--- js/search-script.js
+++ js/search-script.js
@@ -114,3 +114,18 @@ let revealTreeFrog = () => {
         displayFlex($('#treefrog'));
     }
 };
+
+
+// SLOTH MENU
+
+$('.hamburger').on('click', function(){
+    $('nav').animate({left: 0});
+  });
+  
+$('.close-nav').on('click', function(){
+    $('nav').animate({left: '100%'});
+  });
+  
+$(window).on('resize', function(){
+    $('nav').removeAttr('style');
+  });
