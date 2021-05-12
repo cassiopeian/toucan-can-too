1d5698ac02d5c0b60233116a4f9a6add752596ef
cassiopeia <cassiopeia.neely@gmail.com>
2021-05-11 21:07:12 -0400

    update sloth menu
    Minor fix, to align the closing curly braces.

--- js/search-script.js
+++ js/search-script.js
@@ -140,12 +140,12 @@ let revealTreeFrog = () => {
 
 $('.hamburger').on('click', function(){
     $('nav').animate({left: 0});
-  });
+});
   
 $('.close-nav').on('click', function(){
     $('nav').animate({left: '100%'});
-  });
+});
   
 $(window).on('resize', function(){
     $('nav').removeAttr('style');
-  });
+});
