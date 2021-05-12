150a6baa4c7e47b912090effd3058951ae8c962d
cassiopeia <cassiopeia.neely@gmail.com>
2021-05-11 21:05:33 -0400

    .found buttons
    I updated each reveal function to include a line that will add the class .found to the respective animal's button.
    
    Then, I created a button on click that will toggle the display of the next animal facts div, if the clicked button has the class .found.

--- js/search-script.js
+++ js/search-script.js
@@ -44,6 +44,14 @@ $('.random-letter').on('click', function() {
 
 // REVEAL THE HIDDEN ANIMAL FACTS
 
+$('button').on('click', function() {
+    // if a button has the class 'found'
+    if ($(this).hasClass('found')) {
+        // toggle the display of the next animal facts div 
+        $(this).next('div').toggle();
+    }
+});
+
 // this confirms that a letter has been selected/turned green
 let green = (element) => {
     return element.hasClass('selected-letter');
@@ -58,60 +66,70 @@ let displayFacts = (animal) => {
 let revealAnaconda = () => {
     if (green($('#10-1')) && green($('#3-8'))) {
         displayFacts($('#anaconda'));
+        $('#anaconda-button').addClass('found');
     }
 };
 
 let revealCaiman = () => {
     if (green($('#10-11')) && green($('#6-7')) && green($('#5-6'))) {
         displayFacts($('#caiman'));
+        $('#caiman-button').addClass('found');
     }
 };
 
 let revealMacaw = () => {
     if (green($('#2-3')) && green($('#2-7'))) {
         displayFacts($('#macaw'));
+        $('#macaw-button').addClass('found');
     }
 };
 
 let revealMorpho = () => {
     if (green($('#5-5')) && green($('#10-5'))) {
         displayFacts($('#morpho'));
+        $('#morpho-button').addClass('found');
     }
 };
 
 let revealOcelot = () => {
     if (green($('#3-11')) && green($('#8-11'))) {
         displayFacts($('#ocelot'));
+        $('#ocelot-button').addClass('found');
     }
 };
 
 let revealPiranha = () => {
     if (green($('#4-1')) && green($('#9-1')) && green($('#10-1'))) {
         displayFacts($('#piranha'));
+        $('#piranha-button').addClass('found');
     }
 };
 
 let revealSloth = () => {
     if (green($('#7-2')) && green($('#3-2'))) {
         displayFacts($('#sloth'));
+        $('#sloth-button').addClass('found');
     }
 };
 
 let revealTapir = () => {
     if (green($('#5-9')) && green($('#9-9'))) {
         displayFacts($('#tapir'));
+        $('#tapir-button').addClass('found');
     }
 };
 
 let revealToucan = () => {
     if (green($('#6-10')) && green($('#1-10'))) {
         displayFacts($('#toucan'));
+        $('#toucan-button').addClass('found');
     }
 };
 
 let revealTreeFrog = () => {
     if (green($('#1-8')) && green($('#1-1'))) {
         displayFacts($('#treefrog'));
+        $('#treefrog-button').addClass('found');
     }
 };
 
