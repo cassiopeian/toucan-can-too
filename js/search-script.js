7d486fed5bb564e744e8aacabdf827afdd0e5bfd
cassiopeia <cassiopeia.neely@gmail.com>
2021-07-02 22:36:57 -0400

    update .plus-minus display
    When the user reveals an info box, the child .plus/minus is set to display:block. If the user closes the info box, the .plus-minus symbol is changed, to indicate that the box can be expanded again.

--- js/search-script.js
+++ js/search-script.js
@@ -50,6 +50,14 @@ $('button').on('click', function() {
         // toggle the display of the next animal facts div 
         $(this).next('div').toggle();
     }
+
+    if ($(this).hasClass('found') && $(this).children('.plus-minus').html() == '-') {
+        // indicate info box can be expanded
+        $(this).children('.plus-minus').html('+');
+    } else if ($(this).children('.plus-minus').html() == '+') {
+        // indicate info box can be closed
+        $(this).children('.plus-minus').html('-');
+    }
 });
 
 // this confirms that a letter has been selected/turned green
@@ -66,7 +74,7 @@ let displayFacts = (animal) => {
 let revealAnaconda = () => {
     if (green($('#10-1')) && green($('#3-8'))) {
         displayFacts($('#anaconda'));
-        $('#anaconda-button').addClass('found');
+        $('#anaconda-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealAnaconda = () => {};
@@ -76,7 +84,7 @@ let revealAnaconda = () => {
 let revealCaiman = () => {
     if (green($('#10-11')) && green($('#6-7')) && green($('#5-6'))) {
         displayFacts($('#caiman'));
-        $('#caiman-button').addClass('found');
+        $('#caiman-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealCaiman = () => {};
@@ -86,7 +94,7 @@ let revealCaiman = () => {
 let revealMacaw = () => {
     if (green($('#2-3')) && green($('#2-7'))) {
         displayFacts($('#macaw'));
-        $('#macaw-button').addClass('found');
+        $('#macaw-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealMacaw = () => {};
@@ -96,7 +104,7 @@ let revealMacaw = () => {
 let revealMorpho = () => {
     if (green($('#5-5')) && green($('#10-5'))) {
         displayFacts($('#morpho'));
-        $('#morpho-button').addClass('found');
+        $('#morpho-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealMorpho = () => {};
@@ -106,7 +114,7 @@ let revealMorpho = () => {
 let revealOcelot = () => {
     if (green($('#3-11')) && green($('#8-11'))) {
         displayFacts($('#ocelot'));
-        $('#ocelot-button').addClass('found');
+        $('#ocelot-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealOcelot = () => {};
@@ -116,7 +124,7 @@ let revealOcelot = () => {
 let revealPiranha = () => {
     if (green($('#4-1')) && green($('#9-1')) && green($('#10-1'))) {
         displayFacts($('#piranha'));
-        $('#piranha-button').addClass('found');
+        $('#piranha-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealPiranha = () => {};
@@ -126,7 +134,7 @@ let revealPiranha = () => {
 let revealSloth = () => {
     if (green($('#7-2')) && green($('#3-2'))) {
         displayFacts($('#sloth'));
-        $('#sloth-button').addClass('found');
+        $('#sloth-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealSloth = () => {};
@@ -136,7 +144,7 @@ let revealSloth = () => {
 let revealTapir = () => {
     if (green($('#5-9')) && green($('#9-9'))) {
         displayFacts($('#tapir'));
-        $('#tapir-button').addClass('found');
+        $('#tapir-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealTapir = () => {};
@@ -146,7 +154,7 @@ let revealTapir = () => {
 let revealToucan = () => {
     if (green($('#6-10')) && green($('#1-10'))) {
         displayFacts($('#toucan'));
-        $('#toucan-button').addClass('found');
+        $('#toucan-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealToucan = () => {};
@@ -156,7 +164,7 @@ let revealToucan = () => {
 let revealTreeFrog = () => {
     if (green($('#1-8')) && green($('#1-1'))) {
         displayFacts($('#treefrog'));
-        $('#treefrog-button').addClass('found');
+        $('#treefrog-button').addClass('found').children('.plus-minus').css('display', 'block');
 
         // prevent facts from opening again, unless toggled by user
         revealTreeFrog = () => {};
